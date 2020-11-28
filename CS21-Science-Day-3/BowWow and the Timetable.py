6c987bffaf923631b26cf743fb73d51c9430d793
ferialahmed <63975253+ferialahmed@users.noreply.github.com>
2020-11-29 01:28:05 +0200

    Update BowWow and the Timetable.py

--- CS21-Science-Day-3/BowWow and the Timetable.py
+++ CS21-Science-Day-3/BowWow and the Timetable.py
@@ -6,5 +6,5 @@ while (4**i)<s:   O(log s to base 4)
     counter+=1   O(log s to base 4)
 print(counter)    1
 
-Then time of complexity is O(log s to the base 4)
+Then time of complexity is O(log n)
 
