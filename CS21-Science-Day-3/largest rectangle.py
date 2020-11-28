0763a17764893d96fddf908386466fe7b61b8776
ferialahmed <63975253+ferialahmed@users.noreply.github.com>
2020-11-29 01:36:28 +0200

    Update largest rectangle.py

--- CS21-Science-Day-3/largest rectangle.py
+++ CS21-Science-Day-3/largest rectangle.py
@@ -1,22 +1,22 @@
 import os
 def largestRectangle(h):
-    lst=[]
-    n=len(h)
-    counter=1
-    for i in range(n):
-        for j in range(i+1,n):
-            if h[i]<=h[j]:
-                counter+=1
-            else:
+    lst=[]          1
+    n=len(h)        1
+    counter=1       1
+    for i in range(n):   n+1
+        for j in range(i+1,n):  n(n-i)
+            if h[i]<=h[j]     n(n-i)
+                counter+=1     n(n-i)     
+            else:              1
                 break
-        for x in range(i-1,-1,-1):
+        for x in range(i-1,-1,-1):  
             if h[i]<=h[x]:
                 counter+=1
-            else:
+            else:                1
                 break
-        lst.append(h[i]*counter)
-        counter = 1
-    return max(lst)
+        lst.append(h[i]*counter) n
+        counter = 1              n
+    return max(lst)              1
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
@@ -30,3 +30,4 @@ if __name__ == '__main__':
 
     fptr.close()
 
+    Then time complexity is O(n^2)
