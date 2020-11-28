a9c255269c925f870260782f2532bc9f3e617a60
ferialahmed <63975253+ferialahmed@users.noreply.github.com>
2020-11-29 00:57:09 +0200

    Update codeforce.py

--- CS21-Science-Day-1/codeforce.py
+++ CS21-Science-Day-1/codeforce.py
@@ -1,10 +1,13 @@
 from fractions import Fraction
-no1,no2=list(map(int,input().split()))
-Max=max(no1,no2)
-prob=6-Max+1
-if prob==6:
-    print("1/1")
-elif prob==0:
-    print("0/1")
-else:
-    print(Fraction(prob,6))
+no1,no2=list(map(int,input().split())) O(1)
+Max=max(no1,no2)                       O(1)
+prob=6-Max+1                           O(1)
+if prob==6: O(1)
+    print("1/1") O(1)
+elif prob==0: O(1)
+    print("0/1") O(1)
+else: O(1)
+    print(Fraction(prob,6)) O(1)
+    
+    
+ Then time complexity is O(1)
