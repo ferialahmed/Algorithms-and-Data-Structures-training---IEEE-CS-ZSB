f8642b9298a65677741282cf94134b399ca272a3
ferialahmed <63975253+ferialahmed@users.noreply.github.com>
2020-11-29 01:27:44 +0200

    Update BowWow and the Timetable.py

--- CS21-Science-Day-3/BowWow and the Timetable.py
+++ CS21-Science-Day-3/BowWow and the Timetable.py
@@ -1,8 +1,10 @@
-s=int(input(),2)
-counter=0
-i=0
-while i>=0 and (2**(2*i))<s:
-    i+=1
-    counter+=1
-print(counter)
+s=int(input(),2)   1
+counter=0         1
+i=0                1
+while (4**i)<s:   O(log s to base 4)
+    i+=1         O(log s to base 4)
+    counter+=1   O(log s to base 4)
+print(counter)    1
+
+Then time of complexity is O(log s to the base 4)
 
