85f0074a1b1c60b88c9312d7bfad2a22983eb772
ferialahmed <63975253+ferialahmed@users.noreply.github.com>
2020-11-29 13:40:50 +0200

    Update snacktower.py

--- CS21-Science-Day-8/snacktower.py
+++ CS21-Science-Day-8/snacktower.py
@@ -1,11 +1,11 @@
-numberOfsnacks=int(input())
-snacksSize=list(map(int,input().split()))
-listOfsize=set()
-maxsize=max(snacksSize)
-for i in range(len(snacksSize)):
-    listOfsize.add(snacksSize[i])
-    while maxsize in listOfsize:
-        print(maxsize,end=" ")
+numberOfsnacks=int(input())               
+snacksSize=list(map(int,input().split()))  
+listOfsize=set()                           
+maxsize=max(snacksSize)                    
+for i in range(len(snacksSize)):           
+    listOfsize.add(snacksSize[i])           
+    while maxsize in listOfsize:            
+        print(maxsize,end=" ")              
         maxsize-=1
     print('')
 
